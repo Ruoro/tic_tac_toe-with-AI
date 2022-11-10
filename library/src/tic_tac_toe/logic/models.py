1628cf6f7515d9ad45a07b028438743aed0834fa
Johnn <33524526+Ruoro@users.noreply.github.com>
2022-11-10 05:45:32 -0500

    to add a min max model and finding the best moves

--- library/src/tic_tac_toe/logic/models.py
+++ library/src/tic_tac_toe/logic/models.py
@@ -1,15 +1,14 @@
 import enum
+import random
 import re
-# from __future__ import annotations
 from dataclasses import dataclass
 from functools import cached_property
 
-from tic_tac_toe.logic.exceptions import InvalidMove
-from tic_tac_toe.logic.validators import validate_grid, validate_game_state
+from tic_tac_toe.logic.exceptions import InvalidMove, UnknownGameScore
+from tic_tac_toe.logic.validators import validate_game_state, validate_grid
 
-    
 WINNING_PATTERNS = (
-    "???......", 
+    "???......",
     "...???...",
     "......???",
     "?..?..?..",
@@ -17,7 +16,6 @@ WINNING_PATTERNS = (
     "..?..?..?",
     "?...?...?",
     "..?.?.?..",
-
 )
 
 
@@ -25,14 +23,14 @@ class Mark(str, enum.Enum):
     CROSS = "X"
     NAUGHT = "O"
 
-@property
-def other(self) -> "Mark":
-    return Mark.CROSS if self is Mark.NAUGHT else Mark.NAUGHT
+    @property
+    def other(self) -> "Mark":
+        return Mark.CROSS if self is Mark.NAUGHT else Mark.NAUGHT
+
 
 @dataclass(frozen=True)
 class Grid:
     cells: str = " " * 9
-
 
     def __post_init__(self) -> None:
         validate_grid(self)
@@ -49,6 +47,7 @@ class Grid:
     def empty_count(self) -> int:
         return self.cells.count(" ")
 
+
 @dataclass(frozen=True)
 class Move:
     mark: Mark
@@ -56,41 +55,24 @@ class Move:
     before_state: "GameState"
     after_state: "GameState"
 
+
 @dataclass(frozen=True)
 class GameState:
     grid: Grid
     starting_mark: Mark = Mark("X")
 
-    def __post_init__ (self) -> None:
+    def __post_init__(self) -> None:
         validate_game_state(self)
-
-    def make_move_to (self, index: int) -> Move:
-        if self.grid.cells[index] != " ":
-            raise InvalidMove ("Cells is not empty")
-        return Move(
-            mark= self.current_mark,
-            cell_index = index,
-            before_state = self, 
-            after_state= GameState(
-                Grid(
-                    self.grid.cells[index] 
-                    + self.current_mark 
-                    + self.grid.cells[index + 1:]
-                ),
-                self.starting_mark,
-            ),
-        )
-
 
     @cached_property
     def current_mark(self) -> Mark:
-        if self.grid.x_count == 0:
+        if self.grid.x_count == self.grid.o_count:
             return self.starting_mark
         else:
             return self.starting_mark.other
 
     @cached_property
-    def game_not_started (self) -> bool:
+    def game_not_started(self) -> bool:
         return self.grid.empty_count == 9
 
     @cached_property
@@ -100,7 +82,7 @@ class GameState:
     @cached_property
     def tie(self) -> bool:
         return self.winner is None and self.grid.empty_count == 0
-    
+
     @cached_property
     def winner(self) -> Mark | None:
         for pattern in WINNING_PATTERNS:
@@ -115,8 +97,7 @@ class GameState:
             for mark in Mark:
                 if re.match(pattern.replace("?", mark), self.grid.cells):
                     return [
-                        match.start()
-                        for match in re.finditer(r"\?", pattern)
+                        match.start() for match in re.finditer(r"\?", pattern)
                     ]
         return []
 
@@ -128,4 +109,35 @@ class GameState:
                 moves.append(self.make_move_to(match.start()))
         return moves
 
-        
+    def make_random_move(self) -> Move | None:
+        try:
+            return random.choice(self.possible_moves)
+        except IndexError:
+            return None
+
+    def make_move_to(self, index: int) -> Move:
+        if self.grid.cells[index] != " ":
+            raise InvalidMove("Cell is not empty")
+        return Move(
+            mark=self.current_mark,
+            cell_index=index,
+            before_state=self,
+            after_state=GameState(
+                Grid(
+                    self.grid.cells[:index]
+                    + self.current_mark
+                    + self.grid.cells[index + 1 :]
+                ),
+                self.starting_mark,
+            ),
+        )
+
+    def evaluate_score(self, mark: Mark) -> int:
+        if self.game_over:
+            if self.tie:
+                return 0
+            if self.winner is mark:
+                return 1
+            else:
+                return -1
+        raise UnknownGameScore("Game is not over yet")
